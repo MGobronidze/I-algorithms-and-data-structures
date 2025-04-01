3c778b152c94aa27ad0894193fa51079ccbd936e
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2025-04-01 10:33:45 +0400

    4th lecture

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-# insertion sort
+# Insertion Sort
 def insertionSort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
@@ -11,53 +11,50 @@ def insertionSort(arr):
             j -= 1
         arr[j + 1] = key
 
-# Driver code to test the function
-if __name__ == "__main__":
-    arr = [12, 11, 13, 5, 6]
-    print("Original array:", arr)
-    
-    insertionSort(arr)
-    
-    print("Sorted array:", arr)
+# Merge Sort
+def merge_sort(arr):
+    if len(arr) > 1:
+        # Splitting the array in half
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
 
-# merge sort
-# def merge_sort(arr):
-#     if len(arr) > 1:
-#         # მასივის გაყოფა შუაში
-#         mid = len(arr) // 2
-#         left_half = arr[:mid]
-#         right_half = arr[mid:]
+        # Recursive call
+        merge_sort(left_half)
+        merge_sort(right_half)
 
-#         # რეკურსიული გამოძახება
-#         merge_sort(left_half)
-#         merge_sort(right_half)
+        i = j = k = 0
 
-#         i = j = k = 0
+        # Merging and sorting elements
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
 
-#         # ელემენტების შერწყმა და დალაგება
-#         while i < len(left_half) and j < len(right_half):
-#             if left_half[i] < right_half[j]:
-#                 arr[k] = left_half[i]
-#                 i += 1
-#             else:
-#                 arr[k] = right_half[j]
-#                 j += 1
-#             k += 1
+        # Copying remaining elements
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
 
-#         # დარჩენილი ელემენტების გადატანა
-#         while i < len(left_half):
-#             arr[k] = left_half[i]
-#             i += 1
-#             k += 1
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
 
-#         while j < len(right_half):
-#             arr[k] = right_half[j]
-#             j += 1
-#             k += 1
+# Testing Merge Sort
+test_arr = [38, 27, 43, 3, 9, 82, 10]
+print("Before Merge Sort:", test_arr)
+merge_sort(test_arr)
+print("After Merge Sort:", test_arr)
+print("\n" + "-"*40 + "\n")
 
-# # დრაივერ კოდი - ტესტირება
-# if __name__ == "__main__":
-#     test_arr = [38, 27, 43, 3, 9, 82, 10]
-#     print("დალაგებამდე:", test_arr)
-#     merge_sort(test_arr)
-#     print("დალაგების შემდეგ:", test_arr)
+# Testing Insertion Sort
+arr = [12, 11, 13, 5, 6]
+print("Before Insertion Sort:", arr)
+insertionSort(arr)
+print("After Insertion Sort:", arr)
